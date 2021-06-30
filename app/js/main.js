2a1ae9337d79963c39542c2eda4a2a213fae4760
QraisinQ <hs1985irin@gmail.com>
2021-06-30 20:55:07 +0300

    added "why" section

--- app/js/main.js
+++ app/js/main.js
@@ -6,11 +6,23 @@ $(function () {
     normalFill: "#A0A0A0",
     ratedFill: "#ffc000",
   });
-});
 
-$(".weekly__slider").slick({
-  prevArrow:
-    '<button class="slick-arrow slick-prev"> <img src="images/content/prev.png" alt=""></button>',
-  nextArrow:
-    '<button class="slick-arrow slick-next"> <img src="images/content/next.png" alt=""></button>',
+  $(".weekly__slider").slick({
+    prevArrow:
+      '<button class="slick-arrow slick-prev"> <img src="images/content/prev.png" alt=""></button>',
+    nextArrow:
+      '<button class="slick-arrow slick-next"> <img src="images/content/next.png" alt=""></button>',
+  });
+
+  if ($(".newest__cards").length) {
+    var mixer = mixitup(".newest__cards");
+  }
+
+  $(".feed__slider").slick({
+    slidesToShow: 3,
+    prevArrow:
+      '<button class="slick-arrow slick-prev"> <img src="images/content/prev.png" alt=""></button>',
+    nextArrow:
+      '<button class="slick-arrow slick-next"> <img src="images/content/next.png" alt=""></button>',
+  });
 });
